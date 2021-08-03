e65aaf206553d8fd4edfc8874c08100986ec06db
webmogilevtsev <webmogilevtsev@ya.com>
2021-08-04 02:26:49 +0500

    fix(index): add filter
    * добавил фильтрацию по периоду

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -4,31 +4,57 @@ import * as moment from 'moment'
 
 import {IHistory} from '../../shared/interfaces'
 import {BuhgalteryService} from '../../shared/buhgaltery.service'
-import {ModalIncomeComponent} from '../../shared/modal-income/modal-income.component'
+import {ModalIncomeComponent, MY_FORMATS} from '../../shared/modal-income/modal-income.component'
 import {ModalConsumptionComponent} from '../../shared/modal-consumption/modal-consumption.component'
 import {Priznak} from 'src/app/shared/interfaces'
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core'
+import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter} from '@angular/material-moment-adapter'
+import {FormControl} from '@angular/forms'
+
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
 export class IndexComponent implements OnInit {
 
+  private historyData: IHistory[] | undefined
   titleAddCategory: string = ''
   isLoading: boolean = false
-  private historyData: IHistory[] | undefined
   incomeValue: number = 0
   consumptionValue: number = 0
   monthName: string = moment(new Date(), 'MM.YYYY').locale('ru').format('MMMM')
   private startOfMonth = moment().startOf('month')
   private endOfMonth = moment().endOf('month')
+  isShowFilter: boolean = false
+  matStartDate = new FormControl(moment().startOf('month'))
+  matEndDate = new FormControl(moment().endOf('month'))
+  counter: number = 0
 
-  constructor(private dialog: MatDialog, private service: BuhgalteryService) {
+  constructor(
+    private dialog: MatDialog,
+    private service: BuhgalteryService,
+    private _adapter: DateAdapter<any>,
+  ) {
   }
 
   ngOnInit(): void {
+    this._adapter.setLocale('ru')
     this.isLoading = true
+    this._updateData()
+  }
+
+  private _updateData(startDate?: Date, endDate?: Date): void {
     this.service.getAllHistory().subscribe((data) => {
       if (data) {
         this.historyData = data.map((res: { payload: { doc: { id: string; data: () => IHistory[]; }; }; }) => {
@@ -37,25 +63,40 @@ export class IndexComponent implements OnInit {
             ...res.payload.doc.data(),
           }
         })
-        this.isLoading = false
-        this.incomeValue = this.historyData ? this.historyData.filter(h => !h.qntyOrWeightNum)
-          .map(hh => Number(hh.price))
-          .reduce((a, b) => Number(a) + Number(b), 0) : 0
-        this.consumptionValue = this.historyData ? this.historyData.filter(h => h.qntyOrWeightNum)
-          .filter(d => {
-            return moment(Object(d.date).seconds * 1000).isBetween(this.startOfMonth, this.endOfMonth) ? d.date : null
-          })
-          .map(hh => {
-              return (
-                hh.qntyOrWeight === 'qnty'
-                  ? Number(hh.qntyOrWeightNum) * Number(hh.price)
-                  : Number(hh.qntyOrWeightNum) / 1000 * Number(hh.price)
-              )
-            },
-          )
-          .reduce((a, b) => a + b, 0) : 0
+        if (startDate && endDate) {
+          this.historyData = this.historyData?.filter(h => moment(Object(h.date).seconds * 1000)
+            .isBetween(
+              moment(startDate),
+              moment(endDate)
+                .hour(23)
+                .minute(59)
+                .second(59)
+                .millisecond(999),
+            ) ? h : null)
+        }
       }
+      this._updateAnalyticCards(this.historyData)
     })
+  }
+
+  private _updateAnalyticCards(data: IHistory[] | undefined): void {
+    this.incomeValue = data ? data.filter(h => !h.qntyOrWeightNum)
+      .map(hh => Number(hh.price))
+      .reduce((a, b) => Number(a) + Number(b), 0) : 0
+    this.consumptionValue = data ? data.filter(h => h.qntyOrWeightNum)
+      .filter(d => {
+        return moment(Object(d.date).seconds * 1000).isBetween(this.startOfMonth, this.endOfMonth) ? d : null
+      })
+      .map(hh => {
+          return (
+            hh.qntyOrWeight === 'qnty'
+              ? Number(hh.qntyOrWeightNum) * Number(hh.price)
+              : Number(hh.qntyOrWeightNum) / 1000 * Number(hh.price)
+          )
+        },
+      )
+      .reduce((a, b) => a + b, 0) : 0
+    this.isLoading = false
   }
 
   onIncome(): void {
@@ -88,5 +129,14 @@ export class IndexComponent implements OnInit {
         this.service.addHistory(result)
       }
     })
+  }
+
+  onSelectDate(): void {
+    if (this.matStartDate.value && this.matEndDate.value) {
+      this._updateData(
+        this.matStartDate.value,
+        this.matEndDate.value,
+      )
+    }
   }
 }
